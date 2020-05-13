898fad5915aa521fc49af890a9cf16ef330d9a2c
Reine0017 <m.fortitudo.fr@gmail.com>
2020-05-13 15:19:48 +0700

    issues app changes

--- src/Issue.js
+++ src/Issue.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Card from "@material-ui/core/Card"
+import CardContent from "@material-ui/core/CardContent"
+import Typography from "@material-ui/core/Typography"
 
 const Issue = (props) => {
-    console.log("props from issue", props)
+    console.log("props from issue", props.issue.fields)
     console.log("props.issue.fields.ID", props.issue.fields.ID)
     return(
         <div>
             {props.issue ? (
             <div>
                 <Card>
-                    <div>ID: {props.issue.fields.ID}</div>
-                    <div>Title: {props.issue.fields.Title}</div>
-                    <div>Description: {props.issue.fields.Description}</div>
-                    <div>Status: {props.issue.fields.Status}</div>
+                    <CardContent>
+                        <Typography gutterBottom variant="h4"> Title: {props.issue.fields.Title}</Typography>
+                        <Typography gutterBottom variant="h3">Status: {props.issue.fields.Status}</Typography>
+                        <Typography>Description: {props.issue.fields.Description}</Typography>
+                        <Typography component="p">ID: {props.issue.fields.ID}</Typography>
+                    </CardContent>
                 </Card>
                 <br></br>
             </div>)
--- src/SearchIssue.js
+++ src/SearchIssue.js
@@ -1,10 +1,6 @@
-//import React, {useState} from 'react';
 import React, {Component} from 'react';
 import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
-//import { makeStyles } from '@material-ui/core/styles';
-//import Button from '@material-ui/core/Button';
-
 import Grid from '@material-ui/core/Grid';
 import * as contentful from 'contentful'
 
@@ -49,7 +45,10 @@ class SearchIssue extends Component {
 
     getIssues = () => {
         client.getEntries({
-            query: this.state.searchString
+            //works for techTags but not issue
+            //content_type: "techTags",
+            query: this.state.searchString,
+            order: 'sys.createdAt'
         })
         .then((response) => {
             this.setState({issues: response.items})
@@ -80,6 +79,7 @@ class SearchIssue extends Component {
                     this.state.issues ? (
                         <div>
                             <TextField
+                                style={{margin: 24}}
                                 id="searchInput" 
                                 label="Search for issues" 
                                 variant="outlined" 
@@ -87,10 +87,10 @@ class SearchIssue extends Component {
                                     input: classes.input,
                                     notchedOutline: classes.notchedOutline}
                                 }}
-                                onChange = {this.onSearchInputChange}
+                                onChange={this.onSearchInputChange}
                             />
 
-                            <Grid container spacing={3}>
+                            <Grid container spacing={5} style={{padding:20}}>
                                 {this.state.issues.map(currentIssue => 
                                     (<Grid key={currentIssue.fields.ID} item xs={12} sm={6} lg={4} xl={3}>
                                         <Issue issue={currentIssue}/>
@@ -99,87 +99,9 @@ class SearchIssue extends Component {
                         </div>
                     ) : "No issues found"
                 }
-
-
                 
             </div>
         );
     }
 }
 export default withStyles(styles)(SearchIssue);
-
-
-// not sure how to make it functional based
-// const useStyles = makeStyles((theme) => ({
-//     notchedOutline: {
-//         borderWidth: "1px",
-//         borderColor: "purple"
-//     },
-
-//     input: {
-//         color: "purple"
-//     }
-//   }));
-
-// export default function SearchIssue() {
-//     const classes = useStyles();
-//     const [issues, setIssues] = useState([]);
-//     const [searchString, setSearchString] = useState('');
-
-//     const getIssues = () => {
-//         client.getEntries({
-//             query: searchString
-//         })
-//         .then((response) => {
-//             setIssues({issues: response.items})
-//             console.log(issues)
-//         })
-//         .catch((error) => {
-//             console.log("error while fetching issues")
-//             console.error(error)
-//         })
-//     }
-
-//     const onSearchInputChange = (event) => {
-//         console.log("Search changed ..." + event.target.value)
-//         if (event.target.value) {
-//             setSearchString({searchString: event.target.value})
-//             console.log(searchString)
-//         } else {
-//             setSearchString({searchString: ''})
-//         }
-//         getIssues();
-//     }
-
-//     return (
-//         <div>
-//             {issues ? 
-//                 (<div>
-//                     <TextField 
-//                         id="searchInput" 
-//                         label="Search" 
-//                         variant="outlined"
-//                         InputProps={{
-//                             classes: {
-//                                 input: classes.input,
-//                                 notchedOutline: classes.notchedOutline
-//                             }
-//                         }}
-//                         onChange = {onSearchInputChange}
-//                     />
-//                     <Grid container>
-//                         {issues.map(currentIssue => (<Grid item><Issue issue={currentIssue}/></Grid>))}
-//                     </Grid>
-//                 </div>)
-//                 : 
-//                 "no issues found"
-//             }
-
-
-                    
-            
-
-//         </div>
-//     );
-// }
-
